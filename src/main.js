8a1230363c28cd0ede4ac3e51c84601d396e70b7
TanayaPatil1 <tdpatil172001@gmail.com>
2023-10-29 20:12:26 +0530

    initial commit

---
+++ src/main.js
@@ -0,0 +1,111 @@
+let shop = document.getElementById("shop");
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((x) => {
+      let { id, name, price, desc, img } = x;
+      let search = basket.find((x) => x.id === id) || [];
+      return `
+    <div id=product-id-${id} class="item">
+        <img width="220" src=${img} alt="">
+        <div class="details">
+          <h3>${name}</h3>
+          <p>${desc}</p>
+          <div class="price-quantity">
+            <h2>$ ${price} </h2>
+            <div class="buttons">
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">
+              ${search.item === undefined ? 0 : search.item}
+              </div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    })
+    .join(""));
+};
+
+generateShop();
+
+/*let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+
+  // console.log(basket);
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};*/
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  // Check if the total number of items in the cart is less than 50 before allowing addition
+  let totalItems = basket.reduce((acc, curr) => acc + curr.item, 0);
+  if (totalItems >= 50) {
+    // You can add a message or handle the scenario where the cart is full (reached 50 items)
+    alert("Maximum items (50) reached in the cart.");
+    return;
+  }
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    // Check if adding one more item would exceed the limit of 50
+    if (totalItems + 1 <= 50) {
+      search.item += 1;
+    } else {
+      // You can add a message or handle the scenario where adding one more item exceeds the limit
+      alert("Maximum items (50) reached in the cart.");
+      return;
+    }
+  }
+
+ // generateCartItems();
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  // console.log(basket);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
